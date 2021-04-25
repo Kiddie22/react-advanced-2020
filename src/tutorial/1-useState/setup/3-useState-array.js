2e9a35c79bd4daa76238c5d721d382b8ec7bfe1d
Kiddie22 <rasula.20191118@iit.ac.lk>
2021-04-25 18:13:06 +0530

    completed 1-4

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,44 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from "react";
+import { data } from "../../../data";
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data);
+  const [buttonValue, setButtonValue] = React.useState("Clear Items");
+
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => person.id !== id);
+    setPeople(newPeople);
+    setButtonValue("Reset Items");
+  };
+
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}> remove </button>
+          </div>
+        );
+      })}
+      <button
+        id="btnID"
+        className="btn"
+        onClick={() => {
+          if (people == data) {
+            setPeople([]);
+            setButtonValue("Reset Items");
+          } else {
+            setPeople(data);
+            setButtonValue("Clear Items");
+          }
+        }}
+      >
+        {buttonValue}
+      </button>
+    </>
+  );
 };
 
 export default UseStateArray;
--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState("");
+  const firstValue = text || "hello world";
+  const secondValue = text && "hello world";
+  const [isError, setIsError] = useState(false);
 
-  return <h2>short circuit</h2>;
+  const setBtn = () => {
+    setText("text");
+  };
+
+  return (
+    <>
+      {/* <h1>{firstValue}</h1> */}
+      {/* <h1>value : {secondValue}</h1> */}
+      <h1>{text || "it's empty"}</h1>
+      {text && <h1>hello world</h1>}
+      <button onClick={() => setBtn()} className="btn">
+        Click
+      </button>
+      <button onClick={() => setIsError(!isError)} className="btn">
+        Toggle Error
+      </button>
+      {isError && <p>there is an error</p>}
+      {isError ? (
+        <p>there is an error...</p>
+      ) : (
+        <div>
+          <p>There is no error</p>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default ShortCircuit;
